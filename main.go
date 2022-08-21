b7409ea6438afa78c38b0b9184d6f1bcc6610d4b
Aziz Urunov <korasdor@gmail.com>
2022-08-22 01:14:43 +0500

    test coomit

--- main.go
+++ main.go
@@ -23,11 +23,12 @@ func main() {
 		logrus.Fatalf("failed to initialize db %s", err.Error())
 	}
 
-	repos := repository.NewRepository(db)
+	repos := repository.∏ry(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	logrus.Println("Starting run server...")
+	logrus.Println("test")
 
 	srv := new(core.Server)
 	if err := srv.Run(utils.GetEnvVar("PORT"), handlers.InitRoutes()); err != nil {
